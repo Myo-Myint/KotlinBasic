b8648603996c3876552b3be5122564cd790a9cd2
Myo Myint Thu <myomyintthu@Myos-MacBook-Air.local>
2023-06-03 13:31:10 +0100

    Clean up the codes a bit

---
+++ src/main/kotlin/ArraysEgs.kt
@@ -0,0 +1,27 @@
+import java.util.*
+
+fun main(){
+    //two ways to create an array
+    //1. using arrof(elments)
+
+    val firsArr = arrayOf("Myo","Mahomoud","Houtz", "Nijjad")
+    println(firsArr.contentToString())
+
+    //2. using arrays constructor => first parameter : arrayLenght and snd parameter :
+    val sndArr = Array(5){ index -> index * 1}
+    println(sndArr.contentToString())
+
+    val array = listOf(1000,1000000,1000000000,1000000000000,1000000000000000,1000000000000000000)
+    val sizes = arrayOf("byte", "kilobyte", "megabyte", "gigabyte",
+        "terabyte", "petabyte", "exabyte")
+    for ((i, value) in array.withIndex()) {
+        println("1 ${sizes[i]} = ${value.toLong()} bytes")
+    }
+
+    val num = Array(5){index -> index + 11}
+    val stringLst : MutableList<String> = mutableListOf()
+    for (eachNum in num){
+        stringLst.add(eachNum.toString())
+    }
+    println(stringLst)
+}
